2009fc2425fa64466e4f4661ded52dea45548bb7
Tab Fitts <tfitts1212@gmail.com>
2017-12-18 10:27:21 -0700

    [vendor/htc] Regenerate makefiles after template change.
    Change-Id: I614ba05760d293d95bfad499b821dc9fc54df4c3

--- pme/Android.mk
+++ pme/Android.mk
@@ -1,4 +1,3 @@
-# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -144,6 +143,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .xml
 include $(BUILD_PREBUILT)
-
 
 endif
